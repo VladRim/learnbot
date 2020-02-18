e0787c6bfb61e16fa8a6bac83701da4791677782
Rimskiy Vlad <vlad_rym@mail.ru>
2020-02-18 23:03:54 +0300

    tranform main() to if __name__ == "__main__" import ephem

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from urllib3.contrib.socks import SOCKSProxyManager
 import logging
 import settings
-
+import ephem
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -15,15 +15,15 @@ def greet_user(bot, update):
     update.message.reply_text(text)
 
 def talk_to_me(bot, update):
-    user_text = "Привет! {}: Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+    user_text = "Привет! {} Ты написал: {}".format(update.message.chat.first_name, update.message.text)
     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
                  update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
 
 
-def main():
-    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+if __name__ == "__main__":
+    mybot = Updater(settings.APY_KEY, request_kwargs=settings.PROXY)
 
     logging.info('Бот запущен')
 
@@ -34,5 +34,3 @@ def main():
     mybot.start_polling()
     mybot.idle()
 
-
-main()
